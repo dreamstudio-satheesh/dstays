410d86efdaadcc28c9934616719d90a8b220515c
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-09-03 14:54:59 +0530

    app

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -27,4 +27,9 @@ class Booking extends Model
     {
         return $this->belongsTo(Customer::class);
     }
+
+    public function property()
+    {
+        return $this->belongsTo(Property::class);
+    }
 }
