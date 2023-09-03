7851535560c01c71aa9bd0fd97ebb130949ebcce
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-09-03 15:17:15 +0530

    app

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -111,7 +111,8 @@
             <div class="col-lg-12">
                 <div class="card card-default">
                     <div class="card-header justify-content-between card-header-border-bottom">
-                        <select class="form-control"  onchange="window.location.href=this.value" >
+                        <select class="form-control"  onchange="if (this.value) window.location.href=this.value" >
+                            <option value="">Select</option>
                             @foreach ($properties as $property)
                                 <option value="/{{ $property->id }}">{{ $property->name }}</option>
                             @endforeach
