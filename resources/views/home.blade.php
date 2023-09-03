6195407aa4f196d8f14a23fe6968f4c3d315699c
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-09-03 13:21:16 +0530

    app

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
                 events: '/get-bookings', // API Endpoint to get booking events
                 selectable: true,
                 selectLongPressDelay: 500,
-                nextDayThreshold: '00:00' ,
+                allDay: false ,
                 select: function(info) {
                     // Here, you can show the modal
                     console.log('Date range selected');
