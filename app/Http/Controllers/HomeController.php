75e2d46ffbaf7d021b6fdcf2c4cb5e1bd86d0046
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-09-04 16:22:20 +0530

    app

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,8 +23,10 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index($id=1)
+    public function index($id)
     {
+        isset($id) ? : $id=1;
+        
         $customers = Customer::all();
         $properties = Property::all();
        
