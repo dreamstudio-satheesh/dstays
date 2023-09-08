8a7cd0ee07adea7cf3d70e3993d8d11c793849f1
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-09-08 18:52:37 +0530

    app

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -216,7 +216,7 @@
 
                                         <div class="col-xs-6 col-md-6 form-group">
                                             <label class="form-label">Booking Type</label>
-                                            <select class="form-control" onchange="showInputBox(this..value)" name="booking_type"  id="booking_type">
+                                            <select class="form-control" onchange="showInputBox(this.value)" name="booking_type"  id="booking_type">
                                                 <option value="">Select</option>
                                                 <option value="Rent">Rent</option>
                                                 <option value="Package">Package</option>
