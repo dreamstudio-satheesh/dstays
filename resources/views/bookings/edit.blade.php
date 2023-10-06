925ea184e9b5d8ce15c2aaa860b56a4ab73fbafb
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-10-06 09:30:18 +0530

    app

--- resources/views/bookings/edit.blade.php
+++ resources/views/bookings/edit.blade.php
@@ -112,7 +112,7 @@
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary" id="saveBooking">Save
+                                <button type="button" class="btn btn-primary" type="submit">Save
                                     Booking</button>
                             </div>
                         </form>
