89622a8b365d379c5ce51056f71d219ea08bb637
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-10-06 06:41:41 +0530

    app

--- resources/views/bookings/edit.blade.php
+++ resources/views/bookings/edit.blade.php
@@ -13,48 +13,7 @@
                     </div>
 
                     <div class="card-body">
-                        <table id="basic-data-table" class="table nowrap" style="width:100%">
-                            <thead>
-                                <tr>
-                                    <th scope="col">ID</th>
-                                    <th scope="col">Customer Name</th>
-                                    <th scope="col">Property</th>
-                                    <th scope="col">date</th>
-                                    <th scope="col">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($bookings as $book)
-                                    <tr>
-                                        <th scope="row"><a href="#" class="fw-semibold">#{{ $book->id }}</a>
-                                        </th>
-                                        <td>{{ $book->customer->name }}</td>
-                                        <td>{{ $book->property->name }}</td>
-                                        <td>{{ $book->check_in }} - {{ $book->check_out }}</td>
-                                        <td>
-
-                                            <div class="d-flex gap-2">
-                                                <!-- Edit Button -->
-                                                <a href="{{ route('bookings.edit', $book->id) }}"
-                                                    class="btn btn-sm btn-primary edit-item-btn">Edit</a>
-                                                &nbsp;  &nbsp;  &nbsp;
-                                                <div class="remove">
-                                                    <form action="{{ route('bookings.destroy', $book->id) }}"
-                                                        method="POST">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button type="submit"
-                                                            class="btn btn-sm btn-primary remove-item-btn">Remove</button>
-                                                    </form>
-                                                </div>
-                                            </div>
-                                        </td>
-
-                                    </tr>
-                                @endforeach
-
-                            </tbody>
-                        </table>
+                        
 
                     </div>
                 </div>
