d814fc89040f8a25ac020ab79fc27b2514feb333
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-11-06 09:37:41 +0530

    app

--- resources/views/expenses/create.blade.php
+++ resources/views/expenses/create.blade.php
@@ -6,7 +6,7 @@
 
         </div>
         <div class="row">
-            <div class="col-12">
+            <div class="col-8">
                 <div class="card card-default">
                     <div class="card-header card-header-border-bottom d-flex justify-content-between">
                         <h2>Add Explenses</h2>
