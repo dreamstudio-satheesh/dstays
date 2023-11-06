6f04665f09cb98b2de9bc15c3f03ab115096a3ff
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-11-06 12:30:02 +0530

    app

--- resources/views/expenses/create.blade.php
+++ resources/views/expenses/create.blade.php
@@ -136,7 +136,7 @@
 
 
                             <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                               
                                 <button type="submit" class="btn btn-primary">Add
                                     Expense</button>
                             </div>
