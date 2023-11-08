3d4a148ba5853a1d8439ab04aa853510119a58d3
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-11-08 08:18:11 +0530

    app

--- resources/views/expenses/create.blade.php
+++ resources/views/expenses/create.blade.php
@@ -95,7 +95,7 @@
                             <div class="row">
                                 <div class="col-xs-12 col-md-10 form-group">
                                     <label for="start_date"> Date</label>
-                                    <input type="expense_date" class="form-control" value="{{ date('Y-m-d') }}" name="date"
+                                    <input type="date" class="form-control" value="{{ date('Y-m-d') }}" name="expense_date"
                                         required>
                                 </div>
 
