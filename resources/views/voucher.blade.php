a1ee2845c8dcf076f2857fae35ce1804badba18a
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-11-21 18:05:02 +0530

    app

--- resources/views/voucher.blade.php
+++ resources/views/voucher.blade.php
@@ -26,8 +26,9 @@
                     {{ \Carbon\Carbon::parse($booking->created_at)->format('d-m-Y') }} </span>
             </div>
             <div class="title">
-                <h4 class="text-30 mb-0 mt-0">{{ $booking->property->name }}</h4>
                 <h5 class="text-20 mb-0 mt-0">Booking Confirmation</h5>
+                <h4 class="text-25 mb-0 mt-0">{{ $booking->property->name }}</h4>
+                
             </div>
         </div>
         <!-- invoice Description -->
